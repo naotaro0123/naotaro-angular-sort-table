9731467b33954665ef654424796a14ec07195b91
naotaro0123 <n.ikewada@gmail.com>
2024-10-29 16:44:13 +0900

    fix: create sheet table

--- src/components/sheet-table/sheet-table.component.ts
+++ src/components/sheet-table/sheet-table.component.ts
@@ -6,6 +6,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import jspreadsheet from 'jspreadsheet-ce';
+import Sortable from 'sortablejs';
 import { columns, User, users } from '../../data/sample-data';
 
 @Component({
@@ -18,7 +19,7 @@ export class SheetTableComponent implements OnInit, OnDestroy {
   columns: string[] = [...columns];
   users: User[] = [...users];
   #jspreadsheet: ReturnType<typeof jspreadsheet> | undefined;
-  // #sortableInstance: Sortable | undefined;
+  #sortableInstance: Sortable | undefined;
   @ViewChild('spreadsheetContainer', { static: true })
   readonly spreadsheetContainer!: ElementRef<HTMLDivElement>;
 
@@ -33,35 +34,38 @@ export class SheetTableComponent implements OnInit, OnDestroy {
       allowInsertRow: false,
       allowManualInsertColumn: false,
       columns: [
-        { title: 'name', width: 160 },
-        { title: 'title', width: 160 },
-        { title: 'mask', width: 160 },
+        { title: '名前', width: 140 },
+        { title: 'タイトル', width: 140 },
+        { title: '説明', width: 160 },
       ],
       data: this.users.map((user) => [user.name!, user.title!, user.mask!]),
     });
 
-    // this.#sortableInstance = new Sortable(
-    //   document.querySelector('.sheet-table > thead > tr')!,
-    //   {
-    //     draggable: 'th',
-    //     direction: 'horizontal',
-    //     onEnd: (e) => {
-    //       const items = e.target.querySelectorAll('th');
+    // this.#jspreadsheet.table.className = 'sort-table';
 
-    //       const newColumns: string[] = [];
-    //       for (let i = 0; i < items.length; i++) {
-    //         const value = items[i].getAttribute('data-id');
-    //         if (value === null) continue;
-    //         newColumns.push(value);
-    //       }
-    //       this.columns = newColumns;
-    //     },
-    //   }
-    // );
+    this.#sortableInstance = new Sortable(
+      document.querySelector('table > thead > tr')!,
+      {
+        draggable: 'th',
+        direction: 'horizontal',
+        onEnd: (e) => {
+          console.log('onEnd:', e);
+          const items = e.target.querySelectorAll('th');
+
+          const newColumns: string[] = [];
+          for (let i = 0; i < items.length; i++) {
+            const value = items[i].getAttribute('data-id');
+            if (value === null) continue;
+            newColumns.push(value);
+          }
+          this.columns = newColumns;
+        },
+      }
+    );
   }
 
   ngOnDestroy(): void {
-    // this.#sortableInstance?.destroy();
+    this.#sortableInstance?.destroy();
     this.#jspreadsheet?.destroy();
   }
 }
