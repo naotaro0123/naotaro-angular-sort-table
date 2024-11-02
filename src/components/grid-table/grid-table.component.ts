530dfc92d04faf54850184ce56c81eb0f991bea8
naotaro0123 <n.ikewada@gmail.com>
2024-11-02 12:12:56 +0900

    fix: add mat menu

--- src/components/grid-table/grid-table.component.ts
+++ src/components/grid-table/grid-table.component.ts
@@ -1,10 +1,11 @@
 import { AfterViewInit, Component, OnDestroy } from '@angular/core';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import Sortable from 'sortablejs';
 import { User, users } from '../../data/sample-data';
-
 @Component({
   selector: 'grid-table',
   standalone: true,
+  imports: [MatMenuModule, MatMenuTrigger],
   templateUrl: './grid-table.component.html',
   styleUrl: './grid-table.component.scss',
 })
@@ -13,6 +14,19 @@ export class GridTableComponent implements AfterViewInit, OnDestroy {
   #sortableInstance: Sortable | undefined;
 
   ngAfterViewInit(): void {
+    // this.#sortableInstance = new Sortable(
+    //   // この下のdrag-rowは不要だった
+    //   document.querySelector('.grid-table > .drag-rows')!,
+    //   {
+    //     handle: '.handle',
+    //     onEnd: (e) => {
+    //       console.log(e);
+    //     },
+    //   }
+    // );
+  }
+
+  menuOpened(): void {
     this.#sortableInstance = new Sortable(
       // この下のdrag-rowは不要だった
       document.querySelector('.grid-table > .drag-rows')!,
@@ -25,7 +39,11 @@ export class GridTableComponent implements AfterViewInit, OnDestroy {
     );
   }
 
-  ngOnDestroy(): void {
+  menuClosed(): void {
     this.#sortableInstance?.destroy();
+  }
+
+  ngOnDestroy(): void {
+    // this.#sortableInstance?.destroy();
   }
 }
