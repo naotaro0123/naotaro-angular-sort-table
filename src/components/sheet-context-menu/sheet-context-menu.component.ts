7cb33f66f8b0cdfea9f79ece2abdbe95970830c1
naotaro0123 <n.ikewada@gmail.com>
2025-04-22 15:46:48 +0900

    fix: contextMenu event

--- src/components/sheet-context-menu/sheet-context-menu.component.ts
+++ src/components/sheet-context-menu/sheet-context-menu.component.ts
@@ -6,7 +6,6 @@ import {
   ViewChild,
 } from '@angular/core';
 import jspreadsheet from 'jspreadsheet-ce';
-import Sortable from 'sortablejs';
 import { User, users } from '../../data/sample-data';
 
 @Component({
@@ -18,7 +17,6 @@ import { User, users } from '../../data/sample-data';
 export class SheetContextMenuComponent implements AfterViewInit, OnDestroy {
   users: User[] = [...users];
   #jspreadsheet: ReturnType<typeof jspreadsheet> | undefined;
-  #sortableInstance: Sortable | undefined;
   @ViewChild('spreadsheetContainer', { static: true })
   readonly spreadsheetContainer!: ElementRef<HTMLDivElement>;
 
@@ -33,29 +31,40 @@ export class SheetContextMenuComponent implements AfterViewInit, OnDestroy {
         { title: '説明', width: 160 },
       ],
       data: this.users.map((user) => [user.name!, user.title!, user.mask!]),
-    });
+      contextMenu(instance, colIndex, rowIndex, event) {
+        const menuList = [];
+        menuList.push({
+          title: 'コピー',
+          shortcut: 'Ctrl + C',
+          // icon: 'copy',
+          onclick: () => {
+            console.log('copy', colIndex, rowIndex);
+            // FIXME: これだとシート全体がコピーされてしまう
+            // instance.copy();
+            // instance.copyData()
+          },
+        });
+        menuList.push({
+          title: 'ペースト',
+          shortcut: 'Ctrl + V',
+          // icon: 'paste',
+          onclick: async () => {
+            const text = await navigator.clipboard.readText();
+            console.log('paste', colIndex, rowIndex, text);
+            // FIXME: これだと一つのシートしかペーストできない
+            instance.paste(Number(colIndex), Number(rowIndex), text);
+          },
+        });
 
-    this.#sortableInstance = new Sortable(
-      this.#jspreadsheet.table.tHead?.querySelector('tr')!,
-      {
-        draggable: 'td',
-        onStart: (e) => {
-          console.log('onStart:', e);
-        },
-        onEnd: (e) => {
-          console.log('onEnd:', e);
-          const { oldIndex, newIndex } = e;
-          this.#jspreadsheet?.moveColumn(
-            (oldIndex ?? 0) - 1,
-            (newIndex ?? 0) - 1
-          );
-        },
-      }
-    );
+        return menuList;
+      },
+      onload(element, instance) {
+        console.log('onload', element, instance);
+      },
+    });
   }
 
   ngOnDestroy(): void {
-    this.#sortableInstance?.destroy();
     this.#jspreadsheet?.destroy();
   }
 }
